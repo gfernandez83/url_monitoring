e2a6c7febb6e2f175e55c7b6f36b385bca5c7977
jjcpagtakhan <jjcpagtakhan@gmail.com>
2018-03-12 11:22:08 +0800

    Added url monitoring script and files

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+
+/* 
+please this script on your index.php file under nginx root directory - usually /var/www/html
+this will convert csv file into a table. referenced from https://www.zimplicit.se/en/knowledge/convert-csv-file-html-table-php.
+*/
+
+$row = 1;
+if (($handle = fopen("/home/janinep/Documents/SWT_Work/CMS_Monitoring/pt_result.csv", "r")) !== FALSE) {
+   
+    echo '<table border="2">';
+   
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        $num = count($data);
+        if ($row == 1) {
+            echo '<thead><tr>';
+        }else{
+            echo '<tr>';
+        }
+       
+        for ($c=0; $c < $num; $c++) {
+            //echo $data[$c] . "<br />\n";
+            if(empty($data[$c])) {
+               $value = "&nbsp;";
+            }else{
+               $value = $data[$c];
+            }
+            if ($row == 1) {
+                echo '<th>'.$value.'</th>';
+            }else{
+                echo '<td>'.$value.'</td>';
+            }
+        }
+       
+        if ($row == 1) {
+            echo '</tr></thead><tbody>';
+        }else{
+            echo '</tr>';
+        }
+        $row++;
+    }
+   
+    echo '</tbody></table>';
+    fclose($handle);
+}
+?>
---
+++ urlcheck.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PATH=/home/user/bin:/home/user/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+PT=/home/user/Documents/URL_Monitoring/pt
+PTO=/home/user/Documents/URL_Monitoring/pt_output
+PTR=/home/user/Documents/URL_Monitoring/pt_result.csv
+a=200;b=301;c=302;d=304
+
+function url_test {
+	cat /dev/null > $PTO
+	while read pt; do
+		curl -o /dev/null --silent --head --write-out '%{http_code}' -g "$pt" >> $PTO
+		echo -e '\t '"$pt" >> $PTO
+	done <$PT
+}
+
+function url_result {
+	cat /dev/null > $PTR
+	echo -e "Request URL;Status Code;Game Vendor" >> $PTR
+	while read pto; do
+		x=$(echo $pto | awk '{print $1}')
+		y=$(echo $pto | awk '{print $2}')
+		if [[ "$x" == "$a" ]] || [[ "$x" == "$b" ]] || [[ "$x" == "$c" ]] || [[ "$x" == "$d" ]]; then
+			echo -e "$y;$x;PT" >> $PTR
+		else
+			true
+		fi
+	done <$PTO
+}
+
+url_test
+url_result
+
+
+
+
+
+
